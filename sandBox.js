1c3cc570a01774b2c0dec6116e1855c8bf1d30ac
Nicolas Urrego <nicurrego@gmail.com>
2024-09-30 19:40:02 +0900

    Contextos de ejecución y scope chain // Clase #23

--- sandBox.js
+++ sandBox.js
@@ -1,33 +1,17 @@
-// Lexical Binding
+// Explica que las variables tienen un scope chain o alcanze segun el bloque al que pertenencen.
 
-const fictionalCharacter = {
-  name: 'Uncle Ben',
-  messageWithTraditionalFuncion: function (message){
-    console.log(`${this.name} says: ${message}`)
-  },
-  messageWithArrowFuncion: (message) => {
-    console.log(`${this.name} says: ${message}`);
-  }
-}
+const userPoints = 150 //bloque1
 
-fictionalCharacter.messageWithTraditionalFuncion('with great power comes great responsibility')
-fictionalCharacter.messageWithArrowFuncion('beware of green goblin')
+function checkAccess(){//bloque2
+  if (userPoints < 100){//bloque3
+    const message = 'Acces denied: Insufficient points!'
+    return message
+  }//bloque3 
+  else {//bloque4
+    const message = 'Acces granted: Enjoy the premim features!'
+    return message
+  }//bloque4
+}//bloque2
 
-// Para que el Arrowfunction pueda ejecutar correctamente el nombre toca crear un enlace directo:
-
-const fictionalCharacter1 = {
-  name: 'Uncle Ben',
-  messageWithTraditionalFuncion: function (message) {
-    console.log(`${this.name} says: ${message}`);
-  },
-  messageWithArrowFuncion: function (message) {
-    const self = this; // Guardamos una referencia a this
-    const arrowFunc = (msg) => {
-      console.log(`${self.name} says: ${msg}`);
-    };
-    arrowFunc(message);
-  }
-};
-
-fictionalCharacter1.messageWithTraditionalFuncion('with great power comes great responsibility');
-fictionalCharacter1.messageWithArrowFuncion('beware of green goblin');
+console.log(checkAccess());
+//bloque1
