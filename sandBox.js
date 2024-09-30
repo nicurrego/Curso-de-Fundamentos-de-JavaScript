a93953fedb4358dbb5afd8711eeef6e04c700b02
Nicolas Urrego <nicurrego@gmail.com>
2024-09-30 16:16:55 +0900

    Loop: do while // Clase#18

--- sandBox.js
+++ sandBox.js
@@ -1,16 +1,18 @@
 /*
 
-while(condition){
+do {
 codigo
+}while(condition)\
 }
 
 */
 
 let contador = 0;
 
-while (contador < 10) {
-  console.log(contador)
+do {
+  console.log(contador);
   contador++;
-  
-}
-// Tener cuidado porque si no se sale del loop se puede bloquear el navegador
+} while (contador < 10);
+
+// Ejecuta el codigo y luego verifica si se cumple la condicion
+// lo cual permite que se ejecute al menos una vez antes de salir del loop.
