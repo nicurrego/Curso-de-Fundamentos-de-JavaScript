4df4f44c010d59f11a09ba7f4434aa8186f2607b
Nicolas Urrego <nicurrego@gmail.com>
2024-09-30 16:12:41 +0900

    Loop: while // Clase #17

--- forIn.js
+++ forIn.js
@@ -1,5 +1,24 @@
-let canasta = ["manzana", "pera", "naranja", "uva"];
+// let canasta = ["manzana", "pera", "naranja", "uva"];
 
-for (fruta of canasta){
-  console.log(fruta); 
+// for (fruta of canasta){
+//   console.log(fruta); 
+// }
+
+const listaDeCompras = {
+  manzana: 5,
+  pera: 3,
+  naranja: 2,
+  uva: 1,
+};
+
+for (fruta in listaDeCompras){
+  console.log(fruta);
+}
+for (fruta in listaDeCompras) {
+      console.log(`${fruta}: ${listaDeCompras[fruta]}`);
+      
+}
+for (fruta of listaDeCompras){
+  console.log(fruta);//el for in no funciona con elementos INNUMERABLES
+  
 }
---
+++ sandBox.js
@@ -0,0 +1,16 @@
+/*
+
+while(condition){
+codigo
+}
+
+*/
+
+let contador = 0;
+
+while (contador < 10) {
+  console.log(contador)
+  contador++;
+  
+}
+// Tener cuidado porque si no se sale del loop se puede bloquear el navegador
