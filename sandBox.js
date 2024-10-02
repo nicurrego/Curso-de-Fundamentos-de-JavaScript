e804cc4aa0c7cf0ad995e158aca4adf0d2b036e7
Nicolas Urrego <nicurrego@gmail.com>
2024-10-02 15:55:07 +0900

    Prototipos y herencias // Clase # 39

--- sandBox.js
+++ sandBox.js
@@ -1,16 +1,3 @@
-// Una clase es una nueva forma de crear objetos.
-// la funcion constructora es util para entender el codigo viajo.
-class Persona {
-  constructor(nombre, edad){
-    this.nombre = nombre;
-    this.edad = edad;
-  }
-  saludar(){
-    console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} years old`);
-  }
-}
+// lo que se genera en la clase padre se puede heredar y trabajar con ellos mas adelante
 
-const persona1 = new Persona("Nicolas", 10002);
-
-console.log(persona1);
-persona1.saludar()
+// MUY BASICO!!!
