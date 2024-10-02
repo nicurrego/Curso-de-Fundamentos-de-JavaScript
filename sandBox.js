cb0af573f0c1cae2035836f3484e3c7431e9d7d0
Nicolas Urrego <nicurrego@gmail.com>
2024-10-02 13:29:01 +0900

    Busqueda de elemntos con find() y findIndex() // Clase # 32

--- sandBox.js
+++ sandBox.js
@@ -78,3 +78,18 @@ const wordFrecuency = words.reduce((accumulator, currentValue) =>{
 },{})
 
 console.log(wordFrecuency);
+const mulipleOf5 = [5, 10, 15, 20]
+const firstNumberGreaterThan10 = mulipleOf5.find(number => number > 10)
+console.log(mulipleOf5);
+console.log(firstNumberGreaterThan10);
+
+
+
+// findIndex ()
+
+const randomNumber = [6, 14, 27, 56, 40]
+const indexNumber = randomNumber.findIndex(number => number > 39) // solamente me devuelbe el primer numero que cumple con la condicion
+
+console.log(randomNumber);
+console.log(indexNumber);
+
