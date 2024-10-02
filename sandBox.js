17262308f40f9648283c4baa7bcc55faa48f808c
Nicolas Urrego <nicurrego@gmail.com>
2024-10-02 16:03:35 +0900

    Herencia en la práctica // Clase # 40

--- sandBox.js
+++ sandBox.js
@@ -1,3 +1,42 @@
 // lo que se genera en la clase padre se puede heredar y trabajar con ellos mas adelante
 
-// MUY BASICO!!!
+// metodos 
+
+// propiedades
+
+
+// prototype
+//   metodos
+//   propiedades
+
+class Animal {
+  constructor(nombre, tipo){
+    this.nombre = nombre;
+    this.tipo = tipo;
+  }
+  emitirSonido(){ // creando un metodo
+    console.log("El animal emite un sonido");
+  }
+}
+
+class Perro extends Animal{  // Palabra clave "extends (classe padre)"
+  constructor(nombre, tipo, raza){
+    super(nombre, tipo); // Palabra clave "super => permite utilizar las propiedades de la clase padre"
+    this.raza = raza;
+  }
+  emitirSonido(){
+    console.log("El perro ladra");
+  }
+  correr(){
+    console.log(`${this.nombre} corre alegremente`);
+  }
+}
+
+const perro1 = new Perro("Bobby", "Perro", "Pug");
+
+console.log(perro1);
+
+perro1.correr();
+perro1.emitirSonido();
+
+
