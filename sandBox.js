544192289f4e4025970e03a0f8e360fa82e19bd9
Nicolas Urrego <nicurrego@gmail.com>
2024-10-02 13:41:41 +0900

    Spread operator: casos de uso // Clase # 34

--- sandBox.js
+++ sandBox.js
@@ -103,3 +103,44 @@ console.log(animals.slice(1,6));
 console.log(animals.slice(-2));
 console.log(animals.slice(2, -1));
 console.log(animals.slice());
+
+// 1. copying an Array
+
+const originalArray = [1, 2, 3, 4, 5]
+const copyOfArray = [...originalArray]
+
+console.log(originalArray);
+console.log(copyOfArray);
+
+// 2. combining Arrays
+
+const array1 = [1, 2, 3]
+const array2 = [4, 5, 6]
+const combinedArray = [...array1, ...array2]
+
+console.log(array1);
+console.log(array2);
+console.log(combinedArray);
+
+// 3. Creating Arrays with Additional Elements 
+
+const baseArray = [1, 2, 3]
+const arrayWithAdditionalElements = [...baseArray, 4, 5, 6]
+
+console.log(baseArray);
+console.log(arrayWithAdditionalElements);
+
+// 4. Pass elements to funcitons
+
+function suma (a, b, c){
+  return a + b + c
+}
+
+const numbersOfFunction = [1, 2, 3]
+const result = suma(...numbersOfFunction)
+
+console.log(result);
+
+
+ 
+
