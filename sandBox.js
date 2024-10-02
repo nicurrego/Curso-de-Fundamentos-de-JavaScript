9f290e96c3ef7613c213539984cd006f9ae285cf
Nicolas Urrego <nicurrego@gmail.com>
2024-10-02 22:06:59 +0900

    Resolviendo el problema de iniciar seion y mostrar el timeline del usuario // Clase # 43

--- sandBox.js
+++ sandBox.js
@@ -1,9 +1,65 @@
-/*
+const userDatabase = [
+  {
+    username: "Andres",
+    password: "123",
+  },
+  {
+    username: "caro",
+    password: "456",
+  },
+  {
+    username: "mariana",
+    password: "789",
+  }
+];
+const usersTimeline = [
+  {
+    username: "Estefany",
+    timeline: "Me encanta Javascript!",
+  },
+  {
+    username: "Oscar",
+    timeline: "Bebeloper es lo mejor!",
+  },
+  {
+    username: "Mariana",
+    timeline: "A mi me gusta más el café que la leche",
+  },
+  {
+    username: "Andres",
+    timeline: "Yo hoy no quiero trabajar",
+  },
+];
 
-this --- class
+function validarUsuario(username, password) {
+  const validUser = userDatabase.find(e => e.username === username)
+  if(validUser){
+    console.log("Usuario válido");
+    if(validUser.password === password){
+      console.log("Contraseña correcta.");
+      console.log("Acceso concedido!");
+      showTimeline(username)
+    } else {
+      console.log("Contraseña incorrecta!");
+      console.log("Acceso denegado");
+    }
+  } else {
+    console.log("Usuario no encontrado.");
+    console.log("Acceso denegado");
+  }
+}
 
-this --- objeto --- class
-*/
+function showTimeline(user) {
+ const conTimeline = usersTimeline.find(e => e.username === user)  
+ if (conTimeline) {
+  console.log("timeline: " + conTimeline.timeline);
+ } else {
+  console.log("Usuario no tiene timeline actualmente.");
+ }
+}
 
-// this es un enlace a la clase que se construye. this-> instance prototype
+const usuario = prompt("ingresa tu nombre de usuario")
+const password = prompt("ingresa tu contraseña")
 
+
+validarUsuario(usuario, password)
