916179ab5614f89d3007fb39b752605cecacd6d0
Nicolas Urrego <nicurrego@gmail.com>
2024-10-03 09:50:25 +0900

    mejoras en el codigo // Clase # 43

--- sandBox.js
+++ sandBox.js
@@ -39,18 +39,32 @@ const usersTimeline = [
   },
 ];
 
-function usuarioExistente(username, password){
-  for (let i = 0; i < userDatabase.length; i++) {
-    if (
-      userDatabase[i].username === username &&
-      userDatabase[i].password === password
-    ) {
-      console.log("Es correcto");
-      break;
-    } else{
-      console.log("No es correcto");
+function validarUsuario(username, password) {
+  const validUser = userDatabase.find(e => e.username === username)
+  if(validUser){
+    console.log("Usuario válido");
+    if(validUser.password === password){
+      console.log("Contraseña correcta.");
+      console.log("Acceso concedido!");
+      showTimeline(username)
+    } else {
+      console.log("Contraseña incorrecta!");
+      console.log("Acceso denegado");
     }
+  } else {
+    console.log("Usuario no encontrado.");
+    console.log("Acceso denegado");
   }
+}
+
+function showTimeline(user) {
+ const conTimeline = usersTimeline.find(e => e.username === user)  
+ if (conTimeline) {
+  console.log("timeline: " + conTimeline.timeline);
+  alert("timeline: " + conTimeline.timeline);
+ } else {
+  console.log("Usuario no tiene timeline actualmente.");
+ }
 }
 
 const usuario = prompt("ingresa tu nombre de usuario")
