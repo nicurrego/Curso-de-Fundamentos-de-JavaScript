e52072de8f4ef1c34bd3f161df1950e32facc976
Nicolas Urrego <nicurrego@gmail.com>
2024-10-03 13:55:58 +0900

    Usando Async y await en JavaScript // clase # 47

--- sandBox.js
+++ sandBox.js
@@ -1,20 +1,20 @@
-const promise = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    let operationSuccesful = true;
-    if (operationSuccesful){
-      resolve("La operacion fue exitosa!");
-    } else {
-      reject("Fallo la operacion");
-    }
-  }, 1000)
-});
+function fetchData() {
+  fetch("https://rickandmortyapi.com/api/character")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error))
+}
 
-promise
-.then((successMessage) => {
-  console.log(successMessage);
-})
-.catch((errorMessage) => {
-  console.log(errorMessage);
-})
+async function fetchData(){
+  try{
+    let response = await fetch("https://rickandmortyapi.com/api/character");
+    let data = await response.json();
+    console.log(data);
+  } catch (error){
+    console.log(error);
+    
+  }
+}
 
-//simulacion de una promesa en donde la operacion es exitosa
+// el async (funcion asincronica) es mas moderna y facil de entender
+//el await es lo que permite que se ejecuten los pasos una vez se culmine el anterior.
