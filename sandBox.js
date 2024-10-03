7da7d0552b97f08df24d94e44740d561f48a1be4
Nicolas Urrego <nicurrego@gmail.com>
2024-10-03 13:18:15 +0900

    ¿Cómo funciona el JavaScript Engine? // clase # 45

--- sandBox.js
+++ sandBox.js
@@ -1,67 +1,18 @@
-const userDatabase = [
-  {
-    username: "Gabriela",
-    password: "g4a4"
-  },
-  {
-    username: "Andres",
-    password: "123",
-  },
-  {
-    username: "caro",
-    password: "456",
-  },
-  {
-    username: "mariana",
-    password: "789",
-  }
-];
-const usersTimeline = [
-   {
-    username: "caro",
-    timeline: "Te amo"
-   },
-  {
-    username: "Estefany",
-    timeline: "Me encanta Javascript!",
-  },
-  {
-    username: "Oscar",
-    timeline: "Bebeloper es lo mejor!",
-  },
-  {
-    username: "Mariana",
-    timeline: "A mi me gusta más el café que la leche",
-  },
-  {
-    username: "Andres",
-    timeline: "Yo hoy no quiero trabajar",
-  },
-];
+// una manera de administrar las tareas para no sobrecargar la memoria es utilizando una web API
 
-function usuarioExistente(username, password){
-  for (let i = 0; i < userDatabase.length; i++) {
-    if (
-      userDatabase[i].username === username &&
-      userDatabase[i].password === password
-    ) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function singIng(username, password) {
-  if (usuarioExistente(username, password)) {
-    alert(`bienvenido a tu cuenta ${username}`)
-    console.log(usersTimeline);
-  } else {
-    alert("Uuuuuuuuuuuups, usuario o contraseña incorrectos!")
-  }
-}
-
-const usuario = prompt("ingresa tu nombre de usuario")
-const password = prompt("ingresa tu contraseña")
+//la cual se encarga de realizar las tareas por separado, mandar el resultado al queue y de ahi se le permite el reingreso al Call stack una vez este tenga disponibilidad de recursos.
 
 
-singIng(usuario, password)
+call stack      web API
++++++++++    +++++++++++++++++++
++       +    +                 +
++       +    +                 +
++       +    +     timer       +
++       +    +                 +
++       +    + /* function()*/ +
++       +    +                 +
++++++++++    +++++++++++++++++++
+
+                     queue
+   loop   <- +++++++++++++++++++++
+             +++++++++++++++++++++
