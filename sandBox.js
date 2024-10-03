4aef56880eeea4536271f69bf9f559ac3d8f8e66
Nicolas Urrego <nicurrego@gmail.com>
2024-10-03 14:11:59 +0900

    ¿Cómo funciona HTTP? // clase # 50

--- sandBox.js
+++ sandBox.js
@@ -3,3 +3,7 @@
 // PATCH & PUT -> corrige o agrega informacion
  
 //DELETE -> delete
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((response) => response.json())
+.then((data) => console.log(data))
